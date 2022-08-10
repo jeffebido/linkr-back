c15c7d69561dd6a5b216e6f3722f17ed430bcb8a
Jefferson Oliveira <jeffebido@hotmail.com>
2022-08-09 22:22:13 -0300

    Ajuste no select para trazer informações o autor do post

--- src/repositories/postsRepository.js
+++ src/repositories/postsRepository.js
@@ -3,7 +3,10 @@ import db from '../db/database.js';
 
 async function getTimelinePosts() {
 
-    return await db.query(`SELECT * FROM posts ORDER BY id ASC limit 20`);
+    return await db.query(`SELECT posts.description, posts.url, users.name AS author, users.picture_url AS picture_url
+                            FROM posts 
+                            JOIN users ON users.id = posts.author_id
+                            ORDER BY posts.id ASC limit 20`);
 }
 
 export const postsRepository = {
