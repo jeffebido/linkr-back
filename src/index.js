f303bd2b36d1c54238b9e7047c15802cf5f8ed86
Jefferson Oliveira <jeffebido@hotmail.com>
2022-08-15 21:57:10 -0300

    Deploy

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ async function main() {
 
   server.use(authRouter);
 
-  const PORT = process.env.PORT || 3333;
+  const PORT = process.env.PORT || 5000;
   server.listen(PORT, () => {
     console.log(`O servidor suviu na porta ${PORT}`);
   });
