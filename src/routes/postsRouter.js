b543ef8e46304769d053ad58b48fc0547eddae35
guilhermesmatiola <guimatiola@gmail.com>
2022-08-18 13:26:46 -0300

    fix in post like

--- src/routes/postsRouter.js
+++ src/routes/postsRouter.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { getTimelinePosts } from '../controllers/postsController.js';
 import  publishPost  from '../controllers/publishController.js';
-
+import { likePost, deslikePost } from "../controllers/likeController.js";
 import { tokenMiddleware } from '../middlewares/tokenMiddleware.js';
 import { haveHashtag } from '../middlewares/timelinerMiddleware.js';
 
@@ -10,5 +10,9 @@ const router = Router();
 
 router.get("/timeline",  tokenMiddleware, getTimelinePosts);
 router.post("/publish",  tokenMiddleware, haveHashtag,  publishPost);
+
+
+router.post("/posts/favorite", tokenMiddleware , likePost);
+router.delete("/posts/favorite/:postId/:userId", tokenMiddleware , deslikePost);
 
 export default router;
